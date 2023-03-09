9ab0d2e5ee2142579b9be00a138f3e582fd63da8
avishaigonen123 <avishaigonen@gmail.com>
2023-03-09 14:54:33 +0200

    the start of Tar2

---
+++ unittests/primitives/PointTest.java
@@ -0,0 +1,25 @@
+package primitives;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PointTest {
+
+    @Test
+    void subtract() {
+
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void distanceSquared() {
+    }
+
+    @Test
+    void distance() {
+    }
+}
