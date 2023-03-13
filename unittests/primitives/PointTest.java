92011d1a438800dbd87e806bcf5cb00ffa30644e
lazyturtel <110930193+lazyturtel@users.noreply.github.com>
2023-03-13 16:49:41 +0200

    testcase for point and vector complete

--- unittests/primitives/PointTest.java
+++ unittests/primitives/PointTest.java
@@ -13,24 +13,61 @@ public class PointTest {
      /** Test method for {@link primitives.Point#subtract(Point)}. */
     @Test
     void testSubtract() {
-        fail("Not yet implemented");
+        Point p1 = new Point(0, 0, 5);
+
+        // ============ Equivalence Partitions Tests ==============
+        // TC01: Test that checks subtraction between points that are not the same
+        Point p2 = new Point(0,0,0);
+        Vector vr = new Vector(0,0,5);
+        assertEquals(p1.subtract(p2), vr, "subtract() wrong result in points that are not the same");
+
+        // =============== Boundary Values Tests ==================
+        // TC11: Test that checks subtraction between points that are the same
+        assertThrows(IllegalArgumentException.class, () -> p1.subtract(p1),
+                "subtract() points that are the same does not throw an exception");
     }
+
 
     /** Test method for {@link primitives.Point#add(Vector)}}. */
     @Test
     void testAdd() {
-        fail("Not yet implemented");
-    }
-    /** Test method for {@link primitives.Point#distanceSquared(Point)}}. */
+        Point p1 = new Point(0, 0, 5);
+        // ============ Equivalence Partitions Tests ==============
+        // TC01: Test that checks add with anything
+        Vector v1 = new Vector(1,0,0);
+        Point pr = new Point(1,0,5);
+        assertEquals(p1.add(v1),pr, "add() wrong result!");
 
+        // =============== Boundary Values Tests ==================
+        // none
+    }
+
+
+    /** Test method for {@link primitives.Point#distanceSquared(Point)}}. */
     @Test
     void testDistanceSquared() {
-        fail("Not yet implemented");
-    }
-    /** Test method for {@link primitives.Point#distance(Point)}}. */
+        Point p1 = new Point(0, 0, 5);
+        Point p2 = new Point(0,0,0);
+        // ============ Equivalence Partitions Tests ==============
+        // TC01: Test that checks distanceSquared with anything
+        double d = 25;
+        assertEquals(p1.distanceSquared(p2), d,0.000001, "distanceSquared() wrong result!");
 
+        // =============== Boundary Values Tests ==================
+        // none
+    }
+
+    /** Test method for {@link primitives.Point#distance(Point)}}. */
     @Test
     void testDistance() {
-        fail("Not yet implemented");
+        Point p1 = new Point(0, 0, 5);
+        Point p2 = new Point(0,0,0);
+        // ============ Equivalence Partitions Tests ==============
+        // TC01: Test that checks distance with anything
+        double d = 5;
+        assertEquals(p1.distance(p2), d,0.000001, "distance() wrong result!");
+
+        // =============== Boundary Values Tests ==================
+        // none
     }
 }
--- unittests/primitives/VectorTest.java
+++ unittests/primitives/VectorTest.java
@@ -12,13 +12,42 @@ import static primitives.Util.isZero;
 public class VectorTest {
     /** Test method for {@link primitives.Vector#add(Vector)}. */
     @Test
-    void testAdd() {        fail("Not yet implemented");
+    void testAdd() {
+        Vector v1 = new Vector(1, 2, 3);
+        // ============ Equivalence Partitions Tests ==============
+
+        // TC01: Test that checks add func with angle that is smaller then 90
+        Vector v2 = new Vector(0, 3, 0);
+        Vector vr = new Vector(1,5,3);
+        assertEquals(v1.add(v2), vr, "add() wrong result in less then 90 degrees");
+
+        // TC02: Test that checks add func with angle that is smaller then 90
+        v2 = new Vector(0,3,-7);
+        vr = new Vector(1,5,-4);
+        assertEquals(v1.add(v2), vr, "add() wrong result in more then 90 degrees");
+
+        // =============== Boundary Values Tests ==================
+        // TC11: test zero vector from add on contrary vectors
+        Vector v3 = new Vector(-2, -4, -6);
+        Vector v4 = new Vector(2,4,6);
+        assertThrows(IllegalArgumentException.class, () -> v3.add(v4),
+            "add() for same length and 180 degrees does not throw an exception");
 
     }
     /** Test method for {@link primitives.Vector#scale(double)}. */
     @Test
-    void testScale() {        fail("Not yet implemented");
+    void testScale() {
+        Vector v1 = new Vector(1, 2, 3);
+        double t = 2;
+        // ============ Equivalence Partitions Tests ==============
+        // TC01: Test that checks scale with anything that isn't 0
+        Vector vr = new Vector(2, 4, 6);
+        assertEquals(v1.scale(t), vr, "scale() wrong result in no 0");
 
+        // =============== Boundary Values Tests ==================
+        // TC11: test that checks scale with 0
+        assertThrows(IllegalArgumentException.class, () -> v1.scale(0),
+                "scale() for scale with 0 does not throw an exception");
     }
     /** Test method for {@link primitives.Vector#dotProduct(Vector)}. */
     @Test
@@ -26,6 +55,21 @@ public class VectorTest {
         Vector v1 = new Vector(1, 2, 3);
         // ============ Equivalence Partitions Tests ==============
 
+        // TC01: Test that checks add func with angle that is smaller then 90
+        Vector v2 = new Vector(0, 3, 0);
+        double rs = 6;
+        assertEquals(v1.dotProduct(v2), rs,0.00001, "dotProduct() wrong result in less then 90 degrees");
+
+        // TC02: Test that checks add func with angle that is smaller then 90
+        v2 = new Vector(0,3,-7);
+        rs = -15;
+        assertEquals(v1.dotProduct(v2), rs,0.00001, "dotProduct() wrong result in more then 90 degrees");
+
+        // =============== Boundary Values Tests ==================
+        // TC11: test zero vector from dot-product of orthogonal vectors
+        v2 = new Vector(0, 3, -2);
+        rs = 0;
+        assertEquals(v1.dotProduct(v2), rs, 0.00001, "dotProduct() wrong result in orthogonal vectors");
     }
 
     /**
@@ -52,19 +96,41 @@ public class VectorTest {
 
     /** Test method for {@link Vector#lengthSquared()}. */
     @Test
-    void testLengthSquared() {        fail("Not yet implemented");
+    void testLengthSquared() {
+        Vector v1 = new Vector(0, 0, 5);
+        // ============ Equivalence Partitions Tests ==============
+        // TC01: Test that checks lengthSquared with anything
+        double d = 25;
+        assertEquals(v1.lengthSquared(), d,0.000001, "lengthSquared() wrong result!");
 
+        // =============== Boundary Values Tests ==================
+        // none
     }
 
     /** Test method for {@link Vector#length()}. */
     @Test
-    void testLength() {        fail("Not yet implemented");
+    void testLength() {
+        Vector v1 = new Vector(0, 0, 5);
+        // ============ Equivalence Partitions Tests ==============
+        // TC01: Test that checks length with anything
+        double d = 5;
+        assertEquals(v1.length(), d,0.000001, "length() wrong result!");
 
+        // =============== Boundary Values Tests ==================
+        // none
     }
 
     /** Test method for {@link Vector#normalize()}. */
     @Test
-    void testNormalize() {        fail("Not yet implemented");
+    void testNormalize() {
+        Vector v1 = new Vector(0, 0, 5);
+        Vector vr = new Vector(0,0,1);
+        // ============ Equivalence Partitions Tests ==============
+        // TC01: Test that checks normalized for everyone!
+        assertEquals(v1.normalize(), vr, "normalize() wrong result!");
+
+        // =============== Boundary Values Tests ==================
+        // none
 
     }
 }
