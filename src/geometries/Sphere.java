70bd565aa28c6d033d88d280ba8305c033d4e826
lazyturtel <110930193+lazyturtel@users.noreply.github.com>
2023-04-19 17:02:17 +0300

    finished the intersecion on Sphere

--- src/geometries/Sphere.java
+++ src/geometries/Sphere.java
@@ -2,6 +2,7 @@ package geometries;
 
 import primitives.Point;
 import primitives.Ray;
+import primitives.Util;
 import primitives.Vector;
 
 import java.util.List;
@@ -14,6 +15,7 @@ public class Sphere extends RadialGeometry {
 
     /**
      * a constructor that initialize the cylinder
+     *
      * @param _radius the radius
      * @param _center the center point of the sphere
      */
@@ -24,6 +26,7 @@ public class Sphere extends RadialGeometry {
 
     /**
      * getter for center
+     *
      * @return the center point
      */
     public Point getCenter() {
@@ -42,8 +45,26 @@ public class Sphere extends RadialGeometry {
      * @param ray the ray we want to intersect with
      * @return the list of the intersections
      */
-    @Override          
+    @Override
     public List<Point> findIntersections(Ray ray) {
+        Vector u = center.subtract(ray.getP0());
+        double tm = Util.alignZero(ray.getDir().dotProduct(u));
+        double d = Math.sqrt(u.lengthSquared() - tm * tm);
+        if (Util.isZero(d - radius) || d > radius)
+            return null;
+        double th = Math.sqrt(radius*radius - d*d);
+        double t1 = tm+th;
+        double t2 = tm-th;
+        if(t1 > 0 && t2 > 0) {
+           return List.of(ray.getP0().add(ray.getDir().scale(t1)),
+                   ray.getP0().add(ray.getDir().scale(t2)));
+        }
+        if(t1 > 0) {
+            return List.of(ray.getP0().add(ray.getDir().scale(t1)));
+        }
+        if(t2 > 0) {
+            return List.of(ray.getP0().add(ray.getDir().scale(t2)));
+        }
         return null;
     }
 }
