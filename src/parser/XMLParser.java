155a4a4eff2e16a2a3834edb1a41cbaab82c8441
avishaigonen123 <avishaigonen@gmail.com>
2023-05-14 16:17:28 +0300

    we did the rotatation bonus

--- src/parser/XMLParser.java
+++ src/parser/XMLParser.java
@@ -21,7 +21,7 @@ import scene.Scene;
 import java.io.File;
 
 public class XMLParser {
-    private Scene scene ;
+    private Scene scene;
     private static final String FOLDER_PATH = System.getProperty("user.dir") + "/XMLFiles";
 
 
@@ -44,9 +44,9 @@ public class XMLParser {
             scene.background = ConvertToColor(backgroundText); // convert it into color, and move it into the scene
 
             Node ambientLightNode = root.getElementsByTagName("ambient-light").item(0); // get the first node, which is ambient light
-            Element ambientLightElement = (Element)ambientLightNode; // convert it to element, in order to see its attributes.
+            Element ambientLightElement = (Element) ambientLightNode; // convert it to element, in order to see its attributes.
             String ambientLightText = ambientLightElement.getAttribute("color"); // get his attribute, color
-            scene.ambientLight = new AmbientLight(ConvertToColor(ambientLightText),new Double3(1,1,1)); // create new ambient light, ka = (1,1,1).
+            scene.ambientLight = new AmbientLight(ConvertToColor(ambientLightText), new Double3(1, 1, 1)); // create new ambient light, ka = (1,1,1).
 
             Node geometriesNode = root.getElementsByTagName("geometries").item(0); // get the node of the geometries
             NodeList list = geometriesNode.getChildNodes(); // get the list of the geometries
@@ -65,7 +65,7 @@ public class XMLParser {
                         String p1Text = element.getAttribute("p1");
                         String p2Text = element.getAttribute("p2");
                         scene.geometries.add(ConvertToTriangle(p0Text, p1Text, p2Text)); // create the triangle
-                    } // we can do it also for other geometries.
+                    }
                 }
             }
 
@@ -77,50 +77,55 @@ public class XMLParser {
 
     /**
      * convert string to sphere
+     *
      * @param center the text that presents center
      * @param radius same for radius
      * @return the sphere
      */
     private Sphere ConvertToSphere(String center, String radius) {
-        String []str1 = center.split(" ");
-        return new Sphere(Double.parseDouble(radius), new Point(Double.parseDouble(str1[0]),Double.parseDouble(str1[1]),Double.parseDouble(str1[2])));
+        String[] str1 = center.split(" ");
+        return new Sphere(Double.parseDouble(radius), new Point(Double.parseDouble(str1[0]), Double.parseDouble(str1[1]), Double.parseDouble(str1[2])));
     }
 
     /**
      * convert string to triangle
+     *
      * @param p1 the text of point 1
      * @param p2 same 2
      * @param p3 same 3
      * @return the triangle
      */
     private Triangle ConvertToTriangle(String p1, String p2, String p3) {
-        String []str1 = p1.split(" ");
-        String []str2 = p2.split(" ");
-        String []str3 = p3.split(" ");
+        String[] str1 = p1.split(" ");
+        String[] str2 = p2.split(" ");
+        String[] str3 = p3.split(" ");
         return new Triangle(
-                new Point(Double.parseDouble(str1[0]),Double.parseDouble(str1[1]),Double.parseDouble(str1[2])),
-                new Point(Double.parseDouble(str2[0]),Double.parseDouble(str2[1]),Double.parseDouble(str2[2])),
-                new Point(Double.parseDouble(str3[0]),Double.parseDouble(str3[1]),Double.parseDouble(str3[2]))
+                new Point(Double.parseDouble(str1[0]), Double.parseDouble(str1[1]), Double.parseDouble(str1[2])),
+                new Point(Double.parseDouble(str2[0]), Double.parseDouble(str2[1]), Double.parseDouble(str2[2])),
+                new Point(Double.parseDouble(str3[0]), Double.parseDouble(str3[1]), Double.parseDouble(str3[2]))
         );
     }
+
     /**
      * help function for convert from string to Double3
+     *
      * @param text the string
      * @return the double3
      */
     private Double3 ConvertToDouble3(String text) {
-        String []str = text.split(" ");
-        return new Double3(Double.parseDouble(str[0]),Double.parseDouble(str[1]),Double.parseDouble(str[2]));
+        String[] str = text.split(" ");
+        return new Double3(Double.parseDouble(str[0]), Double.parseDouble(str[1]), Double.parseDouble(str[2]));
     }
 
     /**
      * help function for convert from string to color
+     *
      * @param double3 the string
      * @return the color
      */
     private Color ConvertToColor(String double3) {
-        String []str1 = double3.split(" ");
+        String[] str1 = double3.split(" ");
 
-        return new Color(Double.parseDouble(str1[0]),Double.parseDouble(str1[1]),Double.parseDouble(str1[2]));
+        return new Color(Double.parseDouble(str1[0]), Double.parseDouble(str1[1]), Double.parseDouble(str1[2]));
     }
 }
--- src/renderer/Camera.java
+++ src/renderer/Camera.java
@@ -7,7 +7,9 @@ import primitives.Ray;
 
 import java.util.MissingResourceException;
 
+import static java.lang.Math.*;
 import static primitives.Util.isZero;
+
 
 /**
  * Class that presents our camera
@@ -240,5 +242,66 @@ public class Camera {
             //  throw new UnsupportedOperationException();   UnReachable statement
         }
 
+    }
+    // bonus method
+
+    /**
+     * function that transformate the camera by moving the point with a vector
+     * @param vec the vector we want to move on
+     * @return this. we want it to be a builder pattern
+     */
+    public Camera Transfromation(Vector vec)
+    {
+        p0 = p0.add(vec); // we moved the point of the camera
+
+        return this;
+    }
+
+    /**
+     * function that rotate the camera around the axis X
+     * @param aCel the angle (cel)
+     * @return this. we want it to be a builder pattern
+     */
+    public Camera RotationOnXaxis(double aCel)
+    {
+        double a = toRadians(aCel);
+        double x = vTo.getX(), y=  vTo.getY(), z = vTo.getZ();
+        vTo = new Vector(x, y*cos(a)-z*sin(a),y*sin(a)+z*cos(a)); // rotation matrix for rotating in the axis
+        x = vUp.getX(); y=  vUp.getY(); z = vUp.getZ();
+        vUp = new Vector(x, y*cos(a)-z*sin(a),y*sin(a)+z*cos(a)); // same what only with the second vector
+        vRight  = vTo.crossProduct(vUp);
+        return this;
+    }
+
+    /**
+     * function that rotate the camera around the axis Y
+     * @param aCel the angle (cel)
+     * @return this. we want it to be a builder pattern
+     */
+    public Camera RotationOnYaxis(double aCel)
+    {
+        double a = toRadians(aCel);
+        double x = vTo.getX(), y=  vTo.getY(), z = vTo.getZ();
+        vTo = new Vector(x*cos(a)+z*sin(a), y,-x*sin(a)+z*cos(a)); // rotation matrix for rotating in the axis
+        x = vUp.getX(); y=  vUp.getY(); z = vUp.getZ();
+        vUp = new Vector(x*cos(a)+z*sin(a), y,-x*sin(a)+z*cos(a)); // same what only with the second vector
+        vRight  = vTo.crossProduct(vUp);
+        return this;
+    }
+
+    /**
+     * function that rotate the camera around the axis Z
+     * @param aCel the angle (cel)
+     * @return this. we want it to be a builder pattern
+     */
+    public Camera RotationOnZaxis(double aCel)
+    {
+        double a = toRadians(aCel);
+        double x = vTo.getX(), y=  vTo.getY(), z = vTo.getZ();
+        vTo = new Vector(x*cos(a)-y*sin(a), x*sin(a)+y*cos(a),z); // rotation matrix for rotating in the axis
+        x = vUp.getX(); y=  vUp.getY(); z = vUp.getZ();
+        vUp = new Vector(x*cos(a)-y*sin(a), x*sin(a)+y*cos(a),z); // same what only with the second vector
+        vRight  = vTo.crossProduct(vUp);
+        return this;
     }
 }
