fbd51da8314ca0e96a0ee95054ebe85122241706
lazyturtel <110930193+lazyturtel@users.noreply.github.com>
2023-06-12 14:57:58 +0300

    added new test

--- src/primitives/Color.java
+++ src/primitives/Color.java
@@ -12,7 +12,8 @@ public class Color {
 
     /** Black color = (0,0,0) */
     public static final Color BLACK = new Color();
-
+    /** White color = (255,255,255) */
+    public static final Color WHITE = new Color(255,255,255);
     /** Blue color = (0,0,255) */
     public static final Color BLUE = new Color(0,0,255);
 
--- unittests/renderer/GlossyAndBlurryTest.java
+++ unittests/renderer/GlossyAndBlurryTest.java
@@ -3,11 +3,9 @@
  */
 package renderer;
 
-import geometries.Geometry;
-import geometries.Polygon;
-import geometries.Sphere;
-import geometries.Triangle;
+import geometries.*;
 import lighting.AmbientLight;
+import lighting.DirectionalLight;
 import lighting.PointLight;
 import lighting.SpotLight;
 import org.junit.jupiter.api.Test;
@@ -88,6 +86,62 @@ public class GlossyAndBlurryTest {
      // camera1.RotationOnZaxis(20);
       camera1.setImageWriter(imageWriter) //
               .setRayTracer(new RayTracerBasic(scene1)) //
+              .renderImage() //
+              .writeToImage(); //
+   }
+   @Test
+   public void GlossySphereTest() {
+      Scene scene3 = new Scene("squars");
+      Camera camera = new Camera(new Point(500, 500, 50), new Vector(-1, -1, 0), new Vector(0, 0, 1)) //
+              .setVPSize(200, 200).setVPDistance(1000);
+      scene3.setBackground(Color.CYAN);
+      Material wall = new Material().setKd(0.05).setKs(0.05).setShininess(60);
+      Material window = new Material().setKd(0.2).setKs(0.5).setKt(0.3).setShininess(60);
+      //    Material door = new Material().setKd(0.05).setKs(0.35).setShininess(60);
+
+      Point p0 = new Point(0, 0, 0), p1 = new Point(-50, 0, 0), p2 = new Point(0, -50, 0);
+      Point p3 = new Point(0, 0, 50), p4 = new Point(-50, 0, 50), p5 = new Point(0, -50, 50);
+      Point p6 = new Point(-50, -50, 100);
+      Point p10 = new Point(-30, 1, 40), p11 = new Point(-30, 1, 20), p12 = new Point(-10, 1, 20), p13 = new Point(-10, 1, 40);
+      Point p14 = new Point(1, -30, 40), p15 = new Point(1, -30, 20), p16 = new Point(1, -10, 20), p17 = new Point(1, -10, 40);
+      // Point p18 = new Point(), p19 = new Point(), p20 = new Point(),p21 = new Point();
+      scene3.setAmbientLight(new AmbientLight(new Color(WHITE), 0.15));
+      scene3.setBackground(Color.CYAN);
+      scene3.geometries.add(
+              //            new Plane(new Point(0,0,0), new Vector(1,0,0)).setEmission(Color.GREEN)
+              new Polygon(p0, p1, p4, p3).setMaterial(wall).setEmission(Color.GRAY)
+              , new Polygon(p0, p2, p5, p3).setMaterial(wall).setEmission(Color.GRAY)
+              , new Triangle(p3, p4, p6).setMaterial(wall).setEmission(Color.RED)
+              , new Triangle(p3, p5, p6).setMaterial(wall).setEmission(Color.RED)
+
+              , new Polygon(p10, p11, p12, p13).setMaterial(window).setEmission(Color.BLUE.scale(0.4))
+              , new Polygon(p14, p15, p16, p17).setMaterial(window).setEmission(Color.BLUE.scale(0.4))
+
+              , new Polygon(new Point(-25, 0, 85), new Point(-25, 0, 55), new Point(-25, -15, 55), new Point(-25, -15, 85)) //
+                      .setMaterial(new Material().setKd(0.05).setKs(0.05).setShininess(60)).setEmission(Color.MAROON),
+              new Sphere(7d, new Point(-25, 7, 95))
+                      .setMaterial(new Material().setKd(0.05).setKs(0.05).setShininess(60).setKt(0.7)).setEmission(Color.GRAY.scale(0.7)),
+              new Sphere(5d, new Point(-25, -7, 108))
+                      .setMaterial(new Material().setKd(0.05).setKs(0.05).setShininess(60).setKt(0.8)).setEmission(Color.GRAY.scale(0.8)),
+              new Sphere(3d, new Point(-25, 5, 115))
+                      .setMaterial(new Material().setKd(0.05).setKs(0.05).setShininess(60).setKt(0.9)).setEmission(Color.GRAY.scale(0.9))
+              //, new Plane(new Point(400,400,40),new Vector(-1,-1,0)).setMaterial(window.setKb(2).setKt(1))// MIRRRORRRRRR
+//                , new Polygon(p18, p19,p20,p21).setEmission(Color.MAROON.scale(0.8))
+      );
+      int a = 20;
+      for (int i = -200;i<200;i+=a)
+         for (int j = -200;j<200;j+=a)
+            if((i/a+j/a)%2==0)
+               scene3.geometries.add(new Polygon(
+                    new Point(i,j,0),
+                    new Point(i,j+a,0),
+                    new Point(i+a,j+a,0),
+                    new Point(i+a,j,0)).setEmission(Color.WHITE));
+      scene3.lights.add(new DirectionalLight(sphereLightColor, new Vector(0,0,-1)));
+      ImageWriter imageWriter = new ImageWriter("GlossySphereTest", 500, 500);
+      // camera1.RotationOnZaxis(20);
+      camera.setImageWriter(imageWriter) //
+              .setRayTracer(new RayTracerBasic(scene3)) //
               .renderImage() //
               .writeToImage(); //
    }
