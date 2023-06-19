77643ce5fe3d9c8d243e7f9755f6ffff1a7a894c
lazyturtel <110930193+lazyturtel@users.noreply.github.com>
2023-06-19 16:48:02 +0300

    added implementision for find intersecion of AABox

--- src/geometries/AABox.java
+++ src/geometries/AABox.java
@@ -3,7 +3,11 @@ package geometries;
 import primitives.Point;
 import primitives.Ray;
 
+import java.util.LinkedList;
 import java.util.List;
+
+import static java.lang.Math.max;
+import static java.lang.Math.min;
 
 /**
  * an Intersectable that represents an axes aliened box
@@ -33,6 +37,24 @@ public class AABox extends Intersectable {
 
     @Override
     public List<GeoPoint> findGeoIntersectionsHelper(Ray ray) {
-        return null;
+        double tx1 = (A.getX() - ray.getP0().getX())/ray.getDir().getX();
+        double tx2 = (B.getX() - ray.getP0().getX())/ray.getDir().getX();
+
+        double tmin = min(tx1, tx2);
+        double tmax = max(tx1, tx2);
+
+        double ty1 = (A.getY() - ray.getP0().getY())/ray.getDir().getY();
+        double ty2 = (B.getY() - ray.getP0().getY())/ray.getDir().getY();
+
+        tmin = max(tmin ,min(ty1, ty2));
+        tmax = min(tmax,max(ty1, ty2));
+
+        double tz1 = (A.getZ() - ray.getP0().getZ())/ray.getDir().getZ();
+        double tz2 = (B.getZ() - ray.getP0().getZ())/ray.getDir().getZ();
+
+        tmin = max(tmin ,min(tz1, tz2));
+        tmax = min(tmax,max(tz1, tz2));
+
+        return tmax >= tmin? new LinkedList<>():null;
     }
 }
